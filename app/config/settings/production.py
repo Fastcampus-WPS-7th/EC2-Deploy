5b559bf85cce6adbc462658825c0be06872c3013
LeeHanYeong <dev@azelf.com>
2018-03-08 12:15:21 +0900

    dev, production에서 CustomS3Storage사용

--- app/config/settings/production.py
+++ app/config/settings/production.py
@@ -10,6 +10,6 @@ DATABASES = secrets['DATABASES']
 WSGI_APPLICATION = 'config.wsgi.production.application'
 
 # Media(User-uploaded files)를 위한 스토리지
-DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+DEFAULT_FILE_STORAGE = 'config.storage.DefaultFileStorage'
 # Static files(collectstatic)를 위한 스토리지
-STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+STATICFILES_STORAGE = 'config.storage.StaticFilesStorage'
